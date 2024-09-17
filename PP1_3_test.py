698e5348e9d52f69370a90d75350bd08fe03e5f4
MrKalisz <72149056+MrKalisz@users.noreply.github.com>
2024-09-16 22:59:14 -0400

    Update PP1_3_test.py

--- PP1_3_test.py
+++ PP1_3_test.py
@@ -1,51 +1,40 @@
 import os.path
 import sys
-from PP1_1 import *
+from PP1_3 import *
 
 def test_q1(capsys):
 
   try:
-    exists = os.path.exists("PP1_1.py")
+    exists = os.path.exists("PP1_3.py")
     assert exists
   except:
     sys.exit()
 
   q1()
   captured = capsys.readouterr()
-  assert captured.out == "Hello World\n"
+  assert captured.out == "* * * * *\n * * * * *\n* * * * *\n * * * * *\n* * * * *\n\n* * * *\n*     *\n*     *\n*     *\n* * * *\n\n* * * * *\n *     *\n  *   *\n   * *\n    *"
 
 def test_q2(capsys):
 
   try:
-    exists = os.path.exists("PP1_1.py")
+    exists = os.path.exists("PP1_3.py")
     assert exists
   except:
     sys.exit()
 
   q2()
   captured = capsys.readouterr()
-  assert captured.out == "1\n2\n3\n4\n5\n"
+  assert captured.out == "Hello World"
 
 def test_q3(capsys):
 
   try:
-    exists = os.path.exists("PP1_1.py")
+    exists = os.path.exists("PP1_3.py")
     assert exists
   except:
     sys.exit()
 
   q3()
   captured = capsys.readouterr()
-  assert captured.out == " Help Me!\n"
+  assert captured.out == '"Man"\n"Board"\n"Man Overboard"\n'
 
-def test_q4(capsys):
-
-  try:
-    exists = os.path.exists("PP1_1.py")
-    assert exists
-  except:
-    sys.exit()
-
-  q4()
-  captured = capsys.readouterr()
-  assert captured.out == "3 x 3 = 9\n"
